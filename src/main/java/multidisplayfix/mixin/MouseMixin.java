424fb5533f45c1f1d1a752742456d103b62bd04b
WhatYouThing <whatuthing@gmail.com>
2025-02-03 22:15:34 +0100

    fix issues with mods that prevent the cursor from resetting in GUI's

--- src/main/java/multidisplayfix/mixin/MouseMixin.java
+++ src/main/java/multidisplayfix/mixin/MouseMixin.java
@@ -3,31 +3,26 @@ package multidisplayfix.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static net.minecraft.client.util.InputUtil.GLFW_CURSOR;
 import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_CAPTURED;
 
 @Mixin(Mouse.class)
 public abstract class MouseMixin {
     @Unique
     MinecraftClient client = MinecraftClient.getInstance();
-    @Shadow
-    private double x;
-    @Shadow
-    private double y;
 
-    @Inject(method = "unlockCursor", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/InputUtil;setCursorParameters(JIDD)V"), cancellable = true)
+    @Inject(method = "unlockCursor", at = @At(value = "TAIL"))
     private void onMouseMoved(CallbackInfo ci) {
         if (client.currentScreen instanceof ChatScreen) {
             return;
         }
-        InputUtil.setCursorParameters(client.getWindow().getHandle(), GLFW_CURSOR_CAPTURED, this.x, this.y);
-        ci.cancel();
+        GLFW.glfwSetInputMode(client.getWindow().getHandle(), GLFW_CURSOR, GLFW_CURSOR_CAPTURED);
     }
 }
