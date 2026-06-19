636ccf3f5e56618acdc27f4f3ec7c97d599ba958
WhatYouThing <whatuthing@gmail.com>
2026-06-19 23:32:43 +0200

    update mod to 26.1.x

--- src/main/java/multidisplayfix/mixin/MouseMixin.java
+++ src/main/java/multidisplayfix/mixin/MouseMixin.java
@@ -1,8 +1,8 @@
 package multidisplayfix.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.Mouse;
-import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
+import net.minecraft.client.gui.screens.ChatScreen;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -10,19 +10,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.minecraft.client.util.InputUtil.GLFW_CURSOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR;
 import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_CAPTURED;
 
-@Mixin(Mouse.class)
+@Mixin(MouseHandler.class)
 public abstract class MouseMixin {
-    @Unique
-    MinecraftClient client = MinecraftClient.getInstance();
 
-    @Inject(method = "unlockCursor", at = @At(value = "TAIL"))
+    @Unique
+    Minecraft client = Minecraft.getInstance();
+
+    @Inject(method = "releaseMouse", at = @At(value = "TAIL"))
     private void onMouseMoved(CallbackInfo ci) {
-        if (client.currentScreen instanceof ChatScreen) {
+        if (client.screen instanceof ChatScreen) {
             return;
         }
-        GLFW.glfwSetInputMode(client.getWindow().getHandle(), GLFW_CURSOR, GLFW_CURSOR_CAPTURED);
+        GLFW.glfwSetInputMode(client.getWindow().handle(), GLFW_CURSOR, GLFW_CURSOR_CAPTURED);
     }
 }
